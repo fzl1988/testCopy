e66d73424ddbed67c5a81d1137233def96e6b99b
fanzhiliang <zhiliang.fan@ucarinc.com>
2017-04-01 18:34:12 +0800

    this is a test

---
+++ test/src/test/concurrentHashMapTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class concurrentHashMapTest {
+
+	/**
+	 * 
+	 * this is a test
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ConcurrentHashMap<Integer, Integer> a = new ConcurrentHashMap<Integer, Integer>(16);
+	}
+}
